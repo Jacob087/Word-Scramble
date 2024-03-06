305e2c63b8f71edba596b86bd17be293db1642a0
Jacob Jennings <jacob_a_jennings@icloud.com>
2024-03-06 12:10:25 +0000

    Added start.txt Made the textfield and input system for users + animation

---
+++ Word Scramble/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Word Scramble
+//
+//  Created by Tesco Labs on 06/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                        ForEach(usedWords, id: \.self) { word in
+                            HStack {
+                                Image(systemName: "\(word.count).circle")
+                                Text(word)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
+        
+        func addNewWord() {
+            let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+            guard answer.count > 0 else { return }
+            
+            // extra validation to come
+            withAnimation {
+                usedWords.insert(answer, at: 0)
+            }
+            
+            newWord = ""
+        }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
+
