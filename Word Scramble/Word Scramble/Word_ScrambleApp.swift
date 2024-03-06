6e9e7e9e03a3e749db950b7ec9fc876138cfa5b0
Jacob087 <162118004+Jacob087@users.noreply.github.com>
2024-03-06 10:48:25 +0000

    Add files via upload

---
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Tesco Labs on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
